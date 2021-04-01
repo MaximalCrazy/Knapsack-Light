a16928ea36d2b2339859458bc5fb34181b646bc4
Timo <timofalkenhain@gmail.com>
2021-04-01 17:57:46 +0200

    Fertig

--- KnapsackLight/Program.cs
+++ KnapsackLight/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KnapsackLight
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kata.KnapsackLight(10, 5, 6, 4, 8);
         }
     }
 
@@ -14,11 +16,36 @@ namespace KnapsackLight
     {
         public static int KnapsackLight(int value1, int weight1, int value2, int weight2, int maxW)
         {
+            var gegenstand1 = new Gegenstand {Wert = value1, Gewicht = weight1};
+            var gegenstand2 = new Gegenstand {Wert = value2, Gewicht = weight2};
 
+            List<Gegenstand> gegenstandsliste = new List<Gegenstand>
+            {
+                gegenstand1,
+                gegenstand2
+            };
 
-            return 0;
+            List<Gegenstand> gegenstandslisteSortiert = gegenstandsliste.OrderByDescending(o=>o.Wert).ToList();
+             
+            int aktuellerWert = 0;
+            int aktuellesGewicht = 0;
+            foreach (var gegenstand in gegenstandslisteSortiert)
+            {
+                if (gegenstand.Gewicht + aktuellesGewicht <= maxW)
+                {
+                    aktuellesGewicht += gegenstand.Gewicht;
+                    aktuellerWert += gegenstand.Wert;
+                }
+            }
+            
+            return aktuellerWert;
         }
     }
 
+    public class Gegenstand
+    {
+        public int Wert { get; set; }
+        public int Gewicht { get; set; }
+    }
 
 }
